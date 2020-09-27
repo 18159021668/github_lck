e55b96bfb4bdf6fc44ee9d83a55c4baf6bef601b
190495718 <lijian@belle.com.cn>
2020-09-27 23:47:20 +0800

    测试github

---
+++ git-demo2/src/main/java/com/lck/second/gitdemo/gitdemo2/GitDemo2Application.java
@@ -0,0 +1,13 @@
+package com.lck.second.gitdemo.gitdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemo2Application.class, args);
+    }
+
+}
---
+++ git-demo2/src/test/java/com/lck/second/gitdemo/gitdemo2/GitDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lck.second.gitdemo.gitdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
